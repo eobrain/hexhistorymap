deb01b6522d337b440767600a630aed3fa2f9511
Eamonn O'Brien-Strain <eobrain@gmail.com>
2025-05-04 20:53:54 -0700

    signs of life

--- make-hex-list.js
+++ make-hex-list.js
@@ -2,19 +2,24 @@ import * as h3 from 'h3-js'
 import { geocode } from './geocode.js'
 import sleep from './sleep.js'
 import isSea from 'is-sea'
+import prevHexes from './hex-list.js'
 
 const res0hexes = h3.getRes0Cells()
-const hexes = res0hexes.map(hex=>h3.cellToChildren(hex,2)).flat()
-
+const hexes = res0hexes.map(hex => h3.cellToChildren(hex, 2)).flat()
 
 async function main () {
   console.log('export default {')
 
   for (const cell of hexes) {
-    const [lat, lon] = h3.cellToLatLng(cell)
+    let { place, lat, lon } = prevHexes[cell] || {}
+    if (!place) {
+      [lat, lon] = h3.cellToLatLng(cell)
+    }
     if (!isSea(lat, lon)) {
-      const place = await geocode(lat, lon)
-      await sleep(1000)
+      if (!place) {
+        place = await geocode(lat, lon)
+        await sleep(1000)
+      }
       console.log(`  "${cell}": {place: "${place}", lat: ${lat}, lon: ${lon}},`)
     }
   }
--- script.js
+++ script.js
@@ -1,1 +1,61 @@
-// ...existing code...
+import hexes from './hex-list.js'
+import { select } from 'https://esm.sh/d3-selection'
+import { geoPath, geoOrthographic, geoGraticule } from 'https://esm.sh/d3-geo'
+import { json } from 'https://esm.sh/d3-fetch'
+
+const geojson = await json('https://gist.githubusercontent.com/d3indepth/f28e1c3a99ea6d84986f35ac8646fac7/raw/c58cede8dab4673c91a3db702d50f7447b373d98/ne_110m_land.json')
+
+const hexFeatures = Object.entries(hexes).map(([cell, { lat, lon, place }]) => {
+  // const [x, y] = h3.cellToPolar(cell)
+  return {
+    type: 'Feature',
+    properties: { name: place },
+    geometry: {
+      type: 'Point',
+      coordinates: [lon, lat]
+    }
+  }
+})
+
+window.setInterval(update, 100)
+
+const context = select('#content canvas')
+  .node()
+  .getContext('2d')
+
+const projection = geoOrthographic()
+  .scale(300)
+
+const geoGenerator = geoPath()
+  .projection(projection)
+  .pointRadius(4)
+  .context(context)
+
+let yaw = 300
+
+function update () {
+  projection.rotate([yaw, -45])
+
+  context.clearRect(0, 0, 800, 600)
+
+  context.lineWidth = 0.5
+  context.strokeStyle = '#333'
+
+  context.beginPath()
+  geoGenerator({ type: 'FeatureCollection', features: geojson.features })
+  context.stroke()
+
+  // Graticule
+  const graticule = geoGraticule()
+  context.beginPath()
+  context.strokeStyle = '#ccc'
+  geoGenerator(graticule())
+  context.stroke()
+
+  // hexes
+  context.beginPath()
+  geoGenerator({ type: 'FeatureCollection', features: hexFeatures })
+  context.stroke()
+
+  yaw -= 0.2
+}
