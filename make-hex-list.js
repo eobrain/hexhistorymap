4ebc60512a3b491c3ca523c82e1b9c0ceba6f233
Eamonn O'Brien-Strain <eobrain@gmail.com>
2025-05-04 17:47:48 -0700

    Partial run

--- make-hex-list.js
+++ make-hex-list.js
@@ -3,10 +3,14 @@ import { geocode } from './geocode.js'
 import sleep from './sleep.js'
 import isSea from 'is-sea'
 
+const res0hexes = h3.getRes0Cells()
+const hexes = res0hexes.map(hex=>h3.cellToChildren(hex,2)).flat()
+
+
 async function main () {
   console.log('export default {')
 
-  for (const cell of h3.getRes0Cells()) {
+  for (const cell of hexes) {
     const [lat, lon] = h3.cellToLatLng(cell)
     if (!isSea(lat, lon)) {
       const place = await geocode(lat, lon)
