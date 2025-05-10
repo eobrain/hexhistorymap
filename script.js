1032a924b30517fef0a5c2f256d9ecca22c937f4
Eamonn O'Brien-Strain <eobrain@gmail.com>
2025-05-09 19:17:49 -0700

    Stationary map

--- script.js
+++ script.js
@@ -1,60 +1,52 @@
 import hexes from './hex-list.js'
 import { select } from 'https://esm.sh/d3-selection'
-import { geoPath, geoOrthographic, geoGraticule } from 'https://esm.sh/d3-geo'
+import { geoPath, geoAzimuthalEqualArea, geoGraticule } from 'https://esm.sh/d3-geo'
 import { json } from 'https://esm.sh/d3-fetch'
+
+/* global h3 */
 
 const geojson = await json('https://gist.githubusercontent.com/d3indepth/f28e1c3a99ea6d84986f35ac8646fac7/raw/c58cede8dab4673c91a3db702d50f7447b373d98/ne_110m_land.json')
 
 const hexFeatures = Object.entries(hexes).map(([cell, { lat, lon, place }]) => {
+  const coordinates = h3.cellsToMultiPolygon([cell], true)[0]
   return {
     type: 'Feature',
     properties: { name: place },
     geometry: {
-      type: 'Point',
-      coordinates: [lon, lat]
+      type: 'Polygon',
+      coordinates
     }
   }
 })
-
-window.setInterval(update, 100)
 
 const context = select('#content canvas')
   .node()
   .getContext('2d')
 
-const projection = geoOrthographic()
-  .scale(300)
+const projection = geoAzimuthalEqualArea().fitSize([800, 800], geojson)
 
 const geoGenerator = geoPath()
   .projection(projection)
   .pointRadius(6.7)
   .context(context)
 
-let yaw = 300
+context.clearRect(0, 0, 800, 600)
 
-function update () {
-  projection.rotate([yaw, -45])
+context.lineWidth = 0.5
+context.strokeStyle = '#333'
 
-  context.clearRect(0, 0, 800, 600)
+context.beginPath()
+geoGenerator({ type: 'FeatureCollection', features: geojson.features })
+context.stroke()
 
-  context.lineWidth = 0.5
-  context.strokeStyle = '#333'
+// Graticule
+const graticule = geoGraticule()
+context.beginPath()
+context.strokeStyle = '#ccc'
+geoGenerator(graticule())
+context.stroke()
 
-  context.beginPath()
-  geoGenerator({ type: 'FeatureCollection', features: geojson.features })
-  context.stroke()
-
-  // Graticule
-  const graticule = geoGraticule()
-  context.beginPath()
-  context.strokeStyle = '#ccc'
-  geoGenerator(graticule())
-  context.stroke()
-
-  // hexes
-  context.beginPath()
-  geoGenerator({ type: 'FeatureCollection', features: hexFeatures })
-  context.stroke()
-
-  yaw -= 0.2
-}
+// hexes
+context.beginPath()
+geoGenerator({ type: 'FeatureCollection', features: hexFeatures })
+context.stroke()
