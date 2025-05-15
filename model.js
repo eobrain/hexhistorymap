c10db6550517904a3c1586dded1426fd4b530d83
Eamonn O'Brien-Strain <eobrain@gmail.com>
2025-05-14 20:21:42 -0700

    Allow missing states

--- model.js
+++ model.js
@@ -12,7 +12,7 @@ const Hex = cellCode => {
   const parent = hexData[base] ? hexData[base][index1] : null
   const isValid = () => parent && parent.hexes[cellCode]
   const place = () => isValid() ? parent.hexes[cellCode].place : null
-  const stateNames = () => isValid() ? [...parent.states, ...parent.hexes[cellCode].states] : []
+  const stateNames = () => isValid() ? [...parent.states || [], ...parent.hexes[cellCode].states || []] : []
   const coordinates = () => h3.cellsToMultiPolygon([cellCode], true)[0]
 
   return { isValid, place, stateNames, coordinates }
