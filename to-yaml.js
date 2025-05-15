f3a505a8482db960ca6a51d4083807b828f62ae8
Eamonn O'Brien-Strain <eobrain@gmail.com>
2025-05-14 20:16:54 -0700

    Tweak YAML formatting

--- to-yaml.js
+++ to-yaml.js
@@ -4,6 +4,8 @@ import { dump } from 'js-yaml'
 import fs from 'fs'
 
 const sortKeys = true
+const noArrayIndent = true
+const options = { sortKeys, noArrayIndent }
 
-fs.writeFileSync('hexes.yaml', dump(hexes), { sortKeys })
-fs.writeFileSync('states.yaml', dump(states), { sortKeys })
+fs.writeFileSync('hexes.yaml', dump(hexes, options))
+fs.writeFileSync('states.yaml', dump(states, options))
