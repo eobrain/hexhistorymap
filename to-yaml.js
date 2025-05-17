d2f537b11c9668810363816c3fe46661ffdaff1a
Eamonn O'Brien-Strain <eobrain@gmail.com>
2025-05-17 14:46:20 -0700

    Aort hexes

--- to-yaml.js
+++ to-yaml.js
@@ -3,9 +3,12 @@ import states from './states.js'
 import { dump } from 'js-yaml'
 import fs from 'fs'
 
-const sortKeys = true
 const noArrayIndent = true
-const options = { sortKeys, noArrayIndent }
+const options = { noArrayIndent }
 
-fs.writeFileSync('hexes.yaml', dump(hexes, options))
+const hexesSorted = Object.fromEntries(
+  Object.entries(hexes).sort((a, b) => a[0].localeCompare(b[0]))
+)
+
+fs.writeFileSync('hexes.yaml', dump(hexesSorted, options))
 fs.writeFileSync('states.yaml', dump(states, options))
