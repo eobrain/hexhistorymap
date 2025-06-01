e25e9088d5a4214e826b5d1157a1fc90a81cff3b
Eamonn O'Brien-Strain <eobrain@gmail.com>
2025-05-31 18:14:16 -0700

    One color per state

--- state-colors.js
+++ state-colors.js
@@ -1,15 +1,16 @@
 import stateData from './states.js'
 import maxichrome from 'maxichrome'
-import MurmurHash3 from 'imurmurhash'
-
-const colors = await maxichrome(100, ['black', 'white'])
+// import MurmurHash3 from 'imurmurhash'
 
 const stateNames = Object.keys(stateData)
 
+const colors = await maxichrome(stateNames.length, ['black', 'white'])
+
 for (let i = 0; i < stateNames.length; ++i) {
   const stateName = stateNames[i]
-  const colorIndex = MurmurHash3(stateName).result() % colors.length
-  stateData[stateName].color = colors[colorIndex]
+  /* const colorIndex = MurmurHash3(stateName).result() % colors.length
+  stateData[stateName].color = colors[colorIndex] */
+  stateData[stateName].color = colors[i]
 }
 
 console.log('export default', JSON.stringify(stateData, null, 2))
